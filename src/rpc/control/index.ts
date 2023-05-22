bdffc50b4f8fcf6a867862878e4c08d83fffcb02
Juan P Lopez <dolcalmi@gmail.com>
2023-05-22 17:58:33 -0500

    fix: remove push to main in update library gh action

--- src/rpc/control/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './get-memory-info'
-export * from './get-rpc-info'
-export * from './help'
-export * from './logging'
-export * from './stop'
-export * from './up-time'
--- src/rpc/mining/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './get-block-template'
-export * from './get-mining-info'
-export * from './get-network-hash-ps'
-export * from './prioritise-transaction'
-export * from './submit-block'
-export * from './submit-header'
