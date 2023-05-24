37b0733ed1cbd092a4e7ca5e5d2da8124a1ed456
Juan P Lopez <dolcalmi@gmail.com>
2023-05-24 16:10:35 -0500

    fix: add json-bigint to parse responses

--- src/rpc-request.ts
+++ src/rpc-request.ts
@@ -1,4 +1,5 @@
 import { request } from 'undici'
+import JSONBigInt from 'json-bigint'
 import { JSONRPCClient as Client } from 'json-rpc-2.0'
 
 import { Json } from './types'
@@ -14,30 +15,35 @@ type ConnectionSettings = {
   timeout?: number
 }
 
+const { parse: parseJson } = JSONBigInt({ storeAsString: true })
+
 const sendRequest = async (payload: Json, clientParams: ConnectionSettings) => {
   const { url, authHeader } = clientParams
   const reqHeaders = {
     Authorization: authHeader,
     'Content-Type': 'application/json',
   }
+
   const { body, statusCode, headers } = await request(url, {
     method: 'POST',
     headers: reqHeaders,
     body: JSON.stringify(payload),
   })
 
+  const responseText = await body.text()
   if (statusCode !== 200) {
     if (statusCode === 401)
       return Promise.reject(new Error('Invalid credentials'))
     const contentType = headers['content-type']
     const response =
-      contentType === 'application/json' ? await body.json() : await body.text()
+      contentType === 'application/json'
+        ? parseJson(responseText)
+        : responseText
     return Promise.reject(response.error || new Error(response))
   }
 
-  client.receive(await body.json())
+  client.receive(parseJson(responseText))
 }
-
 const client = new Client<ConnectionSettings>(sendRequest)
 
 export default async function (cmd: Command, settings: ConnectionSettings) {
