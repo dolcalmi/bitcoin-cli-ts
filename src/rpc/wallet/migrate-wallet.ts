1df6c555f2712601d603701ec13925afb540bb81
dolcalmi <dolcalmi@users.noreply.github.com>
2023-05-27 18:52:42 +0000

    feat: update rpc methods with bitcoin core v24.1

--- src/rpc/wallet/migrate-wallet.ts
+++ src/rpc/wallet/migrate-wallet.ts
@@ -5,10 +5,14 @@ import request from '../../rpc-request'
 
 type MigrateWalletParams = {
   bitcoind: Bitcoind
+  /* The name of the wallet to migrate. If provided both here and in the RPC endpoint, the two must be identical. */
+  wallet_name?: string
+  /* The wallet passphrase */
+  passphrase: string
 }
 
 /**
- * migratewallet
+ * migratewallet ( "wallet_name" "passphrase" )
  *
  * EXPERIMENTAL warning: This call may not work as expected and may be changed in future releases
  * Migrate the wallet to a descriptor wallet.
@@ -16,7 +20,7 @@ type MigrateWalletParams = {
  * The migration process will create a backup of the wallet before migrating. This backup
  * file will be named <wallet name>-<timestamp>.legacy.bak and can be found in the directory
  * for this wallet. In the event of an incorrect migration, the backup can be restored using restorewallet.
- * Requires wallet passphrase to be set with walletpassphrase call if wallet is encrypted.
+ * Encrypted wallets must have the passphrase provided as an argument to this call.
  *
  */
 export function migrateWallet(params: MigrateWalletParams) {
