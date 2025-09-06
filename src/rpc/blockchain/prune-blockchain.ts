00425d94cd8b1401a8b408934b093a9aef3d845f
dolcalmi <dolcalmi@users.noreply.github.com>
2025-09-06 01:45:25 +0000

    feat: update rpc methods with bitcoin core v29.1

--- src/rpc/blockchain/prune-blockchain.ts
+++ src/rpc/blockchain/prune-blockchain.ts
@@ -13,7 +13,9 @@ type PruneBlockchainParams = {
 /**
  * pruneblockchain height
  *
- * pruneblockchain
+ * Attempts to delete block and undo data up to a specified height or timestamp, if eligible for pruning.
+ * Requires `-prune` to be enabled at startup. While pruned data may be re-fetched in some cases (e.g., via `getblockfrompeer`), local deletion is irreversible.
+ *
  */
 export function pruneBlockchain(params: PruneBlockchainParams) {
   const { bitcoind, ...methodParams } = params
